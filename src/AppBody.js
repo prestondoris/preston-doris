018e90f39948e19c221a15b39561e4c4fd25ab38
Preston Doris <prestondoris@gmail.com>
2020-08-29 16:08:26 -0700

    Added about me section below Jumbotron

--- src/AppBody.js
+++ src/AppBody.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Jumbotron, Button} from 'react-bootstrap'
+import { Jumbotron, Container} from 'react-bootstrap'
 import server from './Server-amico.svg'
 
 function AppBody() {
   return (
-    <div style={{ backgroundColor: '#F0F2F5', width: '100%', height: '100vh'}}>
-      <Jumbotron>
-        <h1>Hello, world!</h1>
-        <div>
-          <img style={{width: '50%', margin: '0 auto'}} src={server} alt='person sitting at desk in front of server illustration'></img>
-        </div>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
+    <div style={{ width: '100%', height: '100vh'}}>
+      <Jumbotron fluid style={{ margin: 0}}>
+        <h1 style={{fontSize: '4.5em'}}>Full Stack Software Engineer</h1>
+        <p style={{ fontSize: '1.5em' }}>I love to tackle new challenges and turning the blank page into something beautiful.</p>
+        <img style={{ width: '40%', margin: '0 auto' }} src={server} alt='person sitting at desk in front of server illustration'></img>
       </Jumbotron>
+      <div style={{ width: '100%', height: '30rem', backgroundColor: '#4092FD', margin: '-5em 0 0 0 '}}>
+        <Container style={{ color: 'white' }}>
+          <h1 style={{fontWeight: 'bold', padding: '2em 0 0 0'}}>I'm Preston. Thanks for visting!</h1>
+          <p style={{fontSize: '1.5em', padding: '1em 0 0 0'}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt 
+            in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Container>
+      </div>
     </div>
   )
 }
