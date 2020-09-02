70e8fcd10286e6c2ea6279fa1884952a26176be1
Preston Doris <prestondoris@gmail.com>
2020-09-02 04:37:17 -0700

    Fixed Mobility bug where project cards were not centered

--- src/Projects.js
+++ src/Projects.js
@@ -3,13 +3,6 @@ import ProjectCard from './ProjectCard'
 import {Container} from 'react-bootstrap'
 
 function Projects() {
-  let wrapperStyles = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    margin: '0 auto'
-  }
 
   let projectData = [
     {
@@ -68,7 +61,7 @@ function Projects() {
     <Container style={{marginTop: '.5em'}}>
       <h4 style={{ fontWeight: '900' }}>My Recent Work</h4>
       <h5>Here are a few projects that I have worked on recently.</h5>
-      <div style={wrapperStyles}>
+      <div className='projects'>
         {projects}
       </div>
     </Container>
